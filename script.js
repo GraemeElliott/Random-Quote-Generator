62f6600e006cc1e0751cdd6a4254c97afd295046
graemeelliott <graeme.elliott@dimedout.com>
2019-05-01 22:15:53 +0100

    Random Quote Generator now complete

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-$(document).ready(function(){
-  //API request
-  let randID = Math.floor(Math.random()* 2000);
-  let endpoint = "https://quotesondesign.com/wp-json/posts?filter[id]=" + randID + "&filter[orderby]=rand&callback=";
-  $.getJSON(endpoint, function(data) {
-    let initQuote = data[0];
-    $(".quote").html(initQuote.content);
-    $(".author").html(initQuote.title);
-  });
+$(document).ready(function() {
+
+  var apiURL = "https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?&callback=?";
+
+  function apiCall() {
+    $.getJSON(apiURL, function(json) {
+      $(".quote").html(json["quoteText"]);
+      $(".author").html(json["quoteAuthor"]);
+      $(".twitterButton").click(function() {
+        window.open(href = "https://twitter.com/intent/tweet?text=" + json["quoteText"] + ", " + json["quoteAuthor"]);
+      });
+    });
+  };
+
+  // on page load
+
+  apiCall();
+
+  // on click
+  $(".quoteButton").click(apiCall);
+
 });
